c7e45d43f7f00c57e10ec295d009fc175540c018
manel <manelbenali13@gmail.com>
2023-07-25 15:49:40 +0200

    modifs

--- back-end/src/Form/OrderItemType.php
+++ back-end/src/Form/OrderItemType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class OrderItemType extends AbstractType
