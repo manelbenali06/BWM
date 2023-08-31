8a7eeb020b42415d522fe0ddbea410147f8eafa2
manel <manelbenali13@gmail.com>
2023-08-31 13:16:19 +0200

    modifs

--- back-end/tests/Unit/ProductTest.php
+++ back-end/tests/Unit/ProductTest.php
@@ -6,9 +6,6 @@ class ProductTest extends KernelTestCase
 {
     public function getEntity():Product
     {
-        //La méthode getEntity->définit les valeurs des propriétés :name, description, price et image
-        //->renvoie l'entité créée.
-        
         return (new Product())-> setName( 'Product #1')
             ->setDescription('Description #1')
             ->setPrice(10.99)
@@ -16,16 +13,10 @@ class ProductTest extends KernelTestCase
     }
     public function testEntityIsValid(): void
     {
-        //On verifie l'entité si elle est valide en appellant le service validator
-        //d'abord on reccupere le kernel
         self::bootKernel();
-        //Je créer le container avec getContainer
         $container = static:: getContainer();
         $product = $this->getEntity();
-        //On reccupere les erreurs potentiels via le validator et on lui demande de valider notre entité
-        //si ya des erreurs elles vont etre contenu dans $errors
         $errors = $container->get('validator')->validate($product);
-        //demander que les erreurs = 0
         $this->assertCount(0, $errors);
     }
 }
