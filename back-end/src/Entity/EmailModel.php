54ae4d33545224c652eec07b2392287214fcce71
manel <manelbenali13@gmail.com>
2023-09-13 16:52:24 +0200

    modifs

---
+++ back-end/src/Entity/EmailModel.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Entity;
+
+
+class EmailModel
+{
+    private ?string $title = null;
+    private ?string $subject = null;
+    private ?string $content = null;
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): static
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
